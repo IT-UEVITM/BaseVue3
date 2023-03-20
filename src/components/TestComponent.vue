8f4403cf2a88e56ce3860ffac9d9b29a36027e88
John Marlon <montoyamarlon14@gmail.com>
2023-03-20 12:32:22 -0500

    Se configura linter

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 class="text-center">
+      {{ msg }}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+  props: {
+    msg: {
+
+    }
+  }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <h1 class="text-center">
+      Esta es la página principal
+    </h1>
+    <img
+      alt="Vue logo"
+      class="d-flex mx-auto"
+      src="../assets/logo.png"
+
+    >
+    <testComponent msg="Este es un componente de prueba" />
+       <h1 class="animate__animated animate__bounce text-center">
+        Elemento animado con animate.css
+      </h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import testComponent from '@/components/TestComponent.vue'
+
+export default {
+
+  name: 'HomeView',
+  components: {
+    testComponent
+  }
+}
+</script>
